4de8df7d19d837fd9a28e6f84076a1445849d665
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-07-13 01:03:16 -0400

    chore: expose req in auth config

--- src/config/authjs.config.ts
+++ src/config/authjs.config.ts
@@ -1,25 +1,28 @@
 import GoogleProvider from "@auth/core/providers/google";
 import GithubProvider from "@auth/core/providers/github";
 import { AuthConfig } from "@auth/core";
+import { Request } from "express";
 
-export const getAuthConfig = (): AuthConfig => ({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    GithubProvider({
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      clientId: process.env.GITHUB_CLIENT_ID!,
-    }),
-  ],
-  secret: process.env.AUTH_SECRET!,
-  trustHost: true,
+export const getAuthConfig = (_req: Request): AuthConfig => {
+  return {
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      }),
+      GithubProvider({
+        clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        clientId: process.env.GITHUB_CLIENT_ID!,
+      }),
+    ],
+    secret: process.env.AUTH_SECRET!,
+    trustHost: true,
 
-  // Uncomment for debugging
-  // logger: {
-  //   warn: console.warn,
-  //   error: console.error,
-  //   debug: console.debug,
-  // },
-});
+    // Uncomment for debugging
+    // logger: {
+    //   warn: console.warn,
+    //   error: console.error,
+    //   debug: console.debug,
+    // },
+  };
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,14 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 import { httpApiAdapters } from "../utils/index.js";
 import { Auth } from "@auth/core";
-import { getAuthConfig } from "../config/authjs.config.js";
+import { getAuthConfig } from "../config/index.js";
 
 const router = Router();
 
-router.use("/*", async (req: Request, res: Response) => {
+router.use("/*", async (req, res) => {
   const request = httpApiAdapters.request.fromExpressToFetch(req);
-  const response = await Auth(request, getAuthConfig());
-  httpApiAdapters.response.fromFetchToExpress(response, res);
+  const response = await Auth(request, getAuthConfig(req));
+  await httpApiAdapters.response.fromFetchToExpress(response, res);
 });
 
 export { router as authRouter };
